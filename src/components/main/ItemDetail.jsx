4fa6cc7ebda49718a9dac7d2c404b30ffaf1861a
Florencia Belén De Souza <90360929+PuchikoNyu@users.noreply.github.com>
2022-03-10 23:57:27 -0300

    detalle bis

--- src/components/main/ItemDetail.jsx
+++ src/components/main/ItemDetail.jsx
@@ -4,8 +4,11 @@ const ItemDetail = ({producto}) => {
         <div className="tarjeta">
             <img src={producto.img} alt={producto.nombre} />
             <h3> {producto.nombre} </h3>
-            <p> Tipo: {producto.tipo} </p>
+            <p> Categoria: {producto.categoria} </p>
+            <p> Sub Categoria: {producto.subcategoria} </p>
             <p> Variedad: {producto.variedad} </p>
+            <p> Tipo: {producto.tipo} </p>
+            <p> Precio: ${producto.precio} </p>
         </div>
     )
 }
